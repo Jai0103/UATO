5237c54bb9220cc11e02e01c19595f7decb14713
Jairus <orolazajairus@gmail.com>
2026-07-09 21:20:05 +0800

    Update next.config.ts

--- uapl-lms-flight-log/next.config.ts
+++ uapl-lms-flight-log/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = "UATO";
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProduction ? `/${repoName}` : "",
+  assetPrefix: isProduction ? `/${repoName}/` : "",
   images: {
     unoptimized: true
   },
