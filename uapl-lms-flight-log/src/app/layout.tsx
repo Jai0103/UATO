488570f8581dbab12433f5186f3fb0bdc3171f60
Jairus <orolazajairus@gmail.com>
2026-07-10 00:30:16 +0800

    Update layout.tsx

--- uapl-lms-flight-log/src/app/layout.tsx
+++ uapl-lms-flight-log/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { MessageProvider } from "@/components/message-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MessageProvider>{children}</MessageProvider>
+      </body>
     </html>
   );
 }
