666d4bef83de7ac490b43d9a8394856804d3d02b
Jairus <orolazajairus@gmail.com>
2026-07-11 20:45:58 +0800

    Update page.tsx

--- uapl-lms-flight-log/src/app/page.tsx
+++ uapl-lms-flight-log/src/app/page.tsx
@@ -256,7 +256,7 @@ export default function LoginPage() {
           </div>
 
           <p className="mt-5 text-center text-xs leading-5 text-slate-500">
-            Authorized access only
+            Powered by: Jairus Github Repo
           </p>
         </div>
       </section>
