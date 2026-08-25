da779abad8873cebf8574fc593f57c78f66b8457
Jairus <orolazajairus@gmail.com>
2026-08-25 18:17:22 +0800

    Update supabase.ts

--- uapl-lms-flight-log/src/lib/supabase.ts
+++ uapl-lms-flight-log/src/lib/supabase.ts
@@ -1,12 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabasePublishableKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-if (!supabaseUrl || !supabasePublishableKey) {
-  throw new Error("Missing Supabase environment variables.");
-}
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "";
 
 export const supabase = createClient(
   supabaseUrl,
