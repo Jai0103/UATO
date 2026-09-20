89aa860d11b422eaae1068c80ef0bf6adb8663ac
Jairus <orolazajairus@gmail.com>
2026-09-20 16:30:05 +0800

    Update firebase-client.ts

--- uapl-lms-flight-log/src/lib/firebase-client.ts
+++ uapl-lms-flight-log/src/lib/firebase-client.ts
@@ -3,9 +3,10 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQPxEa19PLjLNhvcSrgrchph-0O7jV68c",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "aga-flight-management-staging.firebaseapp.com",
   projectId: "aga-flight-management-staging",
   storageBucket: "aga-flight-management-staging.firebasestorage.app",
@@ -16,3 +17,4 @@ const firebaseConfig = {
 export const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firestore = getFirestore(firebaseApp);
+export const firebaseFunctions = getFunctions(firebaseApp, "asia-southeast1");
